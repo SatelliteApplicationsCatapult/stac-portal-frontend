7d9fe881fc5f3a35b48f65ba3ca4a0590e5ec44e
James <james.hinton@live.co.uk>
2022-09-05 16:48:09 +0100

    set up front end for stac validation

--- src/pages/Validator/Validator.jsx
+++ src/pages/Validator/Validator.jsx
@@ -6,6 +6,8 @@ import { TextField } from "@mui/material";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import MDButton from "components/MDButton";
+import MDSnackbar from "components/MDSnackbar";
+import MDAlert from "components/MDAlert";
 import Footer from "examples/Footer";
 import Icon from "@mui/material/Icon";
 
@@ -14,9 +16,29 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import { Box } from "@mui/system";
 import { useState } from "react";
+import { CircularProgress } from "@mui/material";
+
+import "./Validator.scss";
 
 const Validator = () => {
+  const [validJSON, setValidJSON] = useState(null);
+  const [validatorResponse, setValidatorResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [alertBox, setAlertBox] = useState({
+    display: false,
+    message: "",
+    severity: "error",
+    icon: "error",
+  });
+
+  const generateErrorMessage = (error) => {
+    console.log(`Full Error: ${error}`);
+    return `Error validating against schema version ${error.version}. ${error.error_type}: ${error.error_message}`;
+  };
+
   const handleSubmit = () => {
+    setAlertBox({ display: false, message: "", severity: "error" });
+    setIsLoading(true);
     let textField = document.getElementById("text-field");
     fetch("http://localhost:5000/validate/json", {
       method: "POST",
@@ -27,48 +49,170 @@ const Validator = () => {
         json: textField.value,
       }),
     })
-      .then((res) => res.json())
+      .then((res) => {
+        setIsLoading(false);
+        return res.json();
+      })
       .then((data) => {
-        console.log(data);
+        let resp = JSON.parse(data[0])[0];
+        setValidJSON(resp.valid_stac);
+        setValidatorResponse(resp);
+
+        if (!resp.valid_stac) {
+          openErrorSB();
+          setAlertBox({
+            display: true,
+            message: generateErrorMessage(resp),
+            severity: "error",
+            icon: "error",
+          });
+        } else {
+          openSuccessSB();
+          setAlertBox({
+            display: true,
+            message: "Valid STAC",
+            severity: "success",
+            icon: "check_circle",
+          });
+        }
+      })
+      .catch((err) => {
+        setValidJSON(false);
+        setValidatorResponse(err);
+        setAlertBox({
+          display: true,
+          message: "Error: " + err,
+          severity: "error",
+          icon: "error",
+        });
       });
   };
+
+  // Success Toast Message
+  const [successSB, setSuccessSB] = useState(false);
+  const openSuccessSB = () => setSuccessSB(true);
+  const closeSuccessSB = () => setSuccessSB(false);
+  const renderSuccessSB = (
+    <MDSnackbar
+      color="success"
+      icon="check"
+      title="Valid STAC"
+      content="The JSON is a valid STAC"
+      datetime="now"
+      open={successSB}
+      onClose={closeSuccessSB}
+      close={closeSuccessSB}
+      bgWhite
+    />
+  );
+
+  // Error Toast Message
+  const [errorSB, setErrorSB] = useState(false);
+  const openErrorSB = () => setErrorSB(true);
+  const closeErrorSB = () => setErrorSB(false);
+  const renderErrorSB = (
+    <MDSnackbar
+      color="error"
+      icon="error"
+      title="Invalid STAC"
+      content="The JSON is not a valid STAC"
+      datetime="now"
+      open={errorSB}
+      onClose={closeErrorSB}
+      close={closeErrorSB}
+      bgWhite
+    />
+  );
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
+      {renderSuccessSB}
+      {renderErrorSB}
       <Grid item xs={12} pt={2}>
         <MDTypography variant="overline" gutterBottom>
           Use the Area Downloader to choose a date range and geographic area to
           download STAC data from.
         </MDTypography>
+        {alertBox.display ? (
+          <MDAlert color={alertBox.severity}>
+            <Icon
+              fontSize="small"
+              sx={{
+                mr: 3,
+              }}
+            >
+              {alertBox.icon}
+            </Icon>
+            <MDTypography variant="overline" color="white">
+              {alertBox.message}
+            </MDTypography>
+          </MDAlert>
+        ) : null}
       </Grid>
       <Box pt={4} display="flex" justifyContent="space-between">
-        <MDButton
-          variant="contained"
-          color="info"
-          onClick={() => {
-            navigator.clipboard.readText().then((text) => {
+        <Box display="flex">
+          <MDButton
+            variant="contained"
+            color="info"
+            sx={{ mr: 4 }}
+            onClick={() => {
+              navigator.clipboard.readText().then((text) => {
+                const textField = document.getElementById("text-field");
+                textField.value = text;
+              });
+            }}
+            startIcon={<Icon>content_paste</Icon>}
+          >
+            {" "}
+            Paste from clipboard
+          </MDButton>
+
+          <MDButton
+            variant="contained"
+            color="info"
+            sx={{ mr: 4 }}
+            onClick={() => {
+              // export
+            }}
+            startIcon={<Icon>save_alt</Icon>}
+          >
+            {" "}
+            Export as JSON
+          </MDButton>
+
+          <MDButton
+            variant="contained"
+            color="info"
+            sx={{ mr: 4 }}
+            onClick={() => {
               const textField = document.getElementById("text-field");
-              textField.value = text;
-            });
-          }}
-          startIcon={<Icon>content_paste</Icon>}
+              textField.value = "";
+            }}
+            startIcon={<Icon>clear</Icon>}
+          >
+            {" "}
+            Clear
+          </MDButton>
+        </Box>
+        <Box
+          display="flex"
+          width="100%"
+          justifyContent="flex-end"
         >
-          {" "}
-          Paste from clipboard
-        </MDButton>
-        <MDButton
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-          startIcon={<Icon>task_alt</Icon>}
-        >
-          Validate JSON
-        </MDButton>
+          <MDButton
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+            startIcon={<Icon>task_alt</Icon>}
+          >
+            Validate STAC
+          </MDButton>
+        </Box>
       </Box>
       <MDBox pb={3}>
         <Grid container spacing={6}>
-          <Grid item xs={12}>
+          <Grid item xs={12} display="flex" justifyContent="center">
             <TextField
               id="text-field"
               placeholder="Paste STAC JSON here"
@@ -76,7 +220,27 @@ const Validator = () => {
               rows={35}
               margin="normal"
               fullWidth
+              onInput={() => {
+                if (alertBox.display) {
+                  setAlertBox({
+                    display: false,
+                    message: "",
+                    severity: "error",
+                  });
+                }
+              }}
+              // className if validJson is true then green if it is false then red if it is null then white and another class that hides if isLoading is true
+              className={`text-field ${
+                validJSON === true
+                  ? "valid"
+                  : validJSON === false
+                  ? "invalid"
+                  : validJSON === null
+                  ? "null"
+                  : ""
+              } ${isLoading ? "hide" : ""}`}
             />
+            {isLoading && <CircularProgress />}
           </Grid>
         </Grid>
       </MDBox>
