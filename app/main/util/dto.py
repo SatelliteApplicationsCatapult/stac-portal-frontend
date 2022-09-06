9463fb775d5af93c3e714db93019a9ca3326f1c2
Ivica <ivica-matic@outlook.com>
2022-09-06 13:40:02 +0100

    Attempting to make dto for post request body

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -34,8 +34,22 @@ class ValidateDto:
         'json': fields.Raw(required=True, description='JSON object to validate'),
     })
 
+
 class StacIngestionStatusDto:
     api = Namespace('stac_ingestion_status', description='stac ingestion status related operations')
-    stac_ingestion_status = api.model('stac_ingestion_status', {
+    stac_ingestion_status_get = api.model('stac_ingestion_status_get', {
         'status_id': fields.String(required=False, description='status id'),
     })
+    stac_ingestion_status_post = api.model('stac_ingestion_status_post', {
+        'newly_stored_collections_count': fields.Integer(required=True, description='newly stored collections count'),
+        'newly_stored_collections': fields.List(fields.String, required=True, description='newly stored collections'),
+        'updated_collections_count': fields.Integer(required=True, description='updated collections count'),
+        'updated_collections': fields.List(fields.String, required=True, description='updated collections'),
+        'newly_stored_item_count': fields.Integer(required=True, description='newly stored item count'),
+        'updated_items_count': fields.Integer(required=True, description='updated items count'),
+        'already_stored_items_count': fields.Integer(required=True, description='already stored items count'),
+    }),
+
+
+
+
