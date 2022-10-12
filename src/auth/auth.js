bbb3842f0bb529c54d944cf4998082ae83251d3f
Ivica <ivica-matic@outlook.com>
2022-10-12 14:27:49 +0100

    Fixed nginx config for broken reloads

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-console.log("intercept module loaded");
+
 async function auth() {
   axios.interceptors.request.use(async(config) => {
     try {
@@ -10,10 +10,8 @@ async function auth() {
       let tokenExpiryDateTime = new Date(tokenExpiryString);
       let now = new Date();
       let timeToExpiry = tokenExpiryDateTime - now;
-      console.log("time to expiry: " + timeToExpiry);
       // if time to expiry is less than 5 minutes, refresh the token
       if (timeToExpiry < 300000) {
-        console.log("refreshing token");
         try {
         await uninterceptedAxiosInstance.get("/.auth/refresh");
         } catch (err) {
@@ -22,13 +20,11 @@ async function auth() {
         let resp2 = await uninterceptedAxiosInstance.get("/.auth/me");
         ad = resp2.data[0];
       }
-
       let id_token = ad.id_token;
       config.headers.Authorization = `Bearer ${id_token}`;
-      console.log("token added to request");
       return config;
     } catch (error) {
-      console.log("Auth details don't exist, probably localhosting the app");
+      console.info("Auth details don't exist, probably localhosting the app");
       return config;
     }
   });
